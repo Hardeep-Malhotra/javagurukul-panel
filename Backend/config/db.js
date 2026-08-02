737bdfbabb865fa4820b56cae58dc38ab7a9fa03
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-08-02 16:50:26 +0530

    fix: remove debug log for MongoDB URI and ensure proper formatting in package.json

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("URI:", process.env.MONGO_URI);
+
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
