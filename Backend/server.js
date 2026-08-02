3d6c54ca94a7becdaecd9712b2c4f556ea68220c
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-08-02 17:29:43 +0530

    fix: update CORS configuration to allow multiple origins and handle requests without origin

--- Backend/server.js
+++ Backend/server.js
@@ -19,16 +19,32 @@ const app = express();
 // Middlewares
 // ==============================
 app.use(express.json());
-
 app.use(cookieParser());
+
+// ==============================
+// CORS Configuration
+// ==============================
+const allowedOrigins = [
+  "http://localhost:5173", // Local Development
+  "https://javagurukul-panel-sigma.vercel.app", // Production Frontend
+];
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin(origin, callback) {
+      // Postman ya server-to-server request
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 
 // ==============================
