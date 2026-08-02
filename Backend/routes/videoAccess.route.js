2d03d153190681ccf9b3c19135332824b496fff8
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-08-02 16:58:04 +0530

    fix: correct import paths for video access controllers

--- Backend/routes/videoAccess.route.js
+++ Backend/routes/videoAccess.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const assignVideoToBatch = require("../controllers/VideoAccess/assignVideo");
-const getVideosForStudent = require("../controllers/videoAccess/getStudentVideos");
-const getBatchCounts = require("../controllers/videoAccess/getBatchCounts");
-const addVideo = require("../controllers/videoAccess/addVideo");
-const getAllVideos = require("../controllers/videoAccess/getAllVideos");
+const getVideosForStudent = require("../controllers/VideoAccess/getStudentVideos");
+const getBatchCounts = require("../controllers/VideoAccess/getBatchCounts");
+const addVideo = require("../controllers/VideoAccess/addVideo");
+const getAllVideos = require("../controllers/VideoAccess/getAllVideos");
 const editVideo = require("../controllers/VideoAccess/editVideo");
 const deleteVideo = require("../controllers/VideoAccess/deleteVideo");
 
